2c5f2aa1caf7d6ab4c14bd8c6c4f32fd69d6a091
Jon duSaint <jdusaint@sugarcrm.com>
2012-03-29 21:01:02 -0800

    Rework VDDClassLoader
    This change cleans up VDDClassLoader a bit and finishes its documentation.
    
    Testing done:
    
    Ran VDD against a plugin with a variety of errors and one with none.  Made
    sure exceptions reported made sense and that everything worked normally
    otherwise.

--- src/org/sugarcrm/voodoodriver/VDDClassLoader.java
+++ src/org/sugarcrm/voodoodriver/VDDClassLoader.java
@@ -1,18 +1,18 @@
 /*
-Copyright 2011-2012 SugarCRM Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-Please see the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2011-2012 SugarCRM Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You
+ * may may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  Please see the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
 package org.sugarcrm.voodoodriver;
 
@@ -21,64 +21,55 @@ import java.io.File;
 import java.io.FileInputStream;
 
 /**
- * Loads an external java class file into a java Class.
+ * Load a VDD plugin class file.
  *
  * @author trichmond
- *
+ * @author Jon duSaint
  */
+
 public class VDDClassLoader extends ClassLoader {
 
-   public VDDClassLoader(ClassLoader parent) {
-      super(parent);
+   /**
+    * Load a class by name from the specified java class file.
+    *
+    * @param className  name of the class to load
+    * @param classFile  Java class file that contains the class to load
+    * @return the plugin {@link Class}
+    * @throws java.io.FileNotFoundException
+    * @throws java.io.IOException
+    */
+
+   public Class<PluginInterface> loadClass(String className, String classFile)
+      throws java.io.FileNotFoundException, java.io.IOException {
+      File classfd = new File(classFile);
+      FileInputStream fin = new FileInputStream(classfd);
+      ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+      int data = fin.read();
+      while (data != -1) {
+         buffer.write(data);
+         data = fin.read();
+      }
+      fin.close();
+
+      return this.loadClass(className, buffer.toByteArray());
    }
+
 
    /**
-    * Loads a class by name from a java class file.
+    * Load the named class from the provided byte array.
     *
-    * @param className The name of the class to load.
-    * @param classFile The java .class file to load into memory.
-    * @return Class
-    * @throws ClassNotFoundException
+    * @param className  name of the class to load
+    * @param classData  byte array containing the compiled class
+    * @return the plugin {@link Class}
     */
-   @SuppressWarnings("unchecked")
-   public Class<PluginInterface> loadClass(String className, String classFile) throws ClassNotFoundException {
-      Class<PluginInterface> result = null;
-      byte[] classData;
 
-      try {
-         File classfd = new File(classFile);
-         FileInputStream fin = new FileInputStream(classfd);
-         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-         int data = fin.read();
-
-          while(data != -1){
-              buffer.write(data);
-              data = fin.read();
-          }
-          fin.close();
-
-          classData = buffer.toByteArray();
-          result = (Class<PluginInterface>)defineClass(className, classData, 0, classData.length);
-
-      } catch (Exception exp) {
-         exp.printStackTrace();
-         result = null;
-      }
-
-      return result;
-   }
-
-   @SuppressWarnings("unchecked")
    public Class<PluginInterface> loadClass(String className, byte[] classData) {
-      Class<PluginInterface> result = null;
+      @SuppressWarnings("unchecked")
+         Class<PluginInterface> cls =
+         (Class<PluginInterface>)defineClass(className, classData,
+                                             0, classData.length);
 
-      try {
-         result = (Class<PluginInterface>)defineClass(className, classData, 0, classData.length);
-      } catch (Exception exp) {
-         exp.printStackTrace();
-         result = null;
-      }
-
-      return result;
+      return cls;
    }
 }
