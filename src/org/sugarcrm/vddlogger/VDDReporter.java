855ef8196bada34e6559645739e7e991ee570f4f
Jon duSaint <jdusaint@sugarcrm.com>
2013-07-31 12:09:04 -0800

    Move VDDReporter's entry point into VDDReporter
    This change finishes the consolidation of VDDReporter's entry point by
    moving main into VDDReporter and deleting VddLogConverter.
    
    Testing done:
    
    Compiled both regular and debug builds and ran VDDReporter with both.

--- src/org/sugarcrm/vddlogger/VDDReporter.java
+++ src/org/sugarcrm/vddlogger/VDDReporter.java
@@ -59,6 +59,69 @@ public class VDDReporter {
 
 
    /**
+    * VDDReporter entry point.
+    *
+    * @param args  command line arguments
+    */
+
+   public static void main(String[] args) {
+      ArrayList<File> xml = null;
+      File dir = null;
+      VddLogCmdParser p = new VddLogCmdParser(args);
+      VddLogCmdOpts opts = p.parse();
+
+      if (opts.containsKey("help")) {
+         System.out.println("Usage:\n" +
+                            "   VDDReporter --suitefile=<suite.xml>\n" +
+                            "   VDDReporter --suitedir=<suite dir>");
+         System.exit(0);
+      }
+
+      if (opts.containsKey("suitefile")) {
+         File f = new File(opts.get("suitefile"));
+
+         if (!f.exists()) {
+            System.out.println("(!)Suite file '" + f + "' does not exist");
+            System.exit(3);
+         }
+
+         System.out.println("(*)Processing suite file: '" + f + "'...");
+         xml = new ArrayList<File>();
+         xml.add(f);
+         dir = f.getAbsoluteFile().getParentFile();
+      } else if (opts.containsKey("suitedir")) {
+         dir = new File(opts.get("suitedir"));
+         System.out.println("(*)Processing suite directory: '" + dir + "'.");
+
+         File fs[] = dir.listFiles(new java.io.FilenameFilter() {
+               public boolean accept(File dir, String name) {
+                  boolean ok = name.toLowerCase().endsWith(".xml");
+                  if (ok) {
+                     System.out.println("(*)Found Suite File: '" + name + "'.");
+                  }
+                  return ok;
+               }
+            });
+
+         if (fs == null) {
+            System.out.println("(!)Suite directory '" + dir + "' is not valid");
+            System.exit(4);
+         }
+
+         xml = new ArrayList<File>(java.util.Arrays.asList(fs));
+
+      } else {
+         System.out.println("(!)Missing --suitefile or --suitedir!");
+         System.exit(2);
+      }
+
+      System.out.println("(*)Generating Summary file...");
+      VDDReporter r = new VDDReporter(xml, dir);
+      r.generateReport();
+   }
+
+
+   /**
     * Instantiate a VDDReporter object
     *
     * @param xmlFiles  list of VDD suite output files
